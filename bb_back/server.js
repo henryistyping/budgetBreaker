aef96746492ffa575a0e4d8e01b5fa292fc17f16
henryistyping <mykim1891@gmail.com>
2023-05-09 21:12:32 -0700

    initialize backend server

---
+++ bb_back/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const app = express()
+
+var corsOptions = {
+  origin: "http://localhost:8000"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: "Welcome to the backend!" })
+})
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
